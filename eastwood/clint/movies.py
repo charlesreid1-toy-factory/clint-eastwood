6c409dc3b7bdc4866fa8c620f81f930a1ee3b95d
Charles Reid <charlesreid1@gmail.com>
2022-09-10 16:20:38 -0700

    expand movies operation subcommands

--- eastwood/clint/movies.py
+++ eastwood/clint/movies.py
@@ -54,3 +54,62 @@ def movies_westerns(argv, args):
         print("\n".join(westerns))
         print()
 
+
+@movies.action(
+    "crime",
+    arguments={
+        "--json": json_flag_options,
+        "--sort": sort_flag_options,
+    },
+)
+def movies_crime(argv, args):
+    """
+    Crime movies starring Clint Eastwood
+    """
+    crimes = [
+        "Mystic River",
+        "Dirty Harry",
+        "Magnum Force",
+        "The Mule",
+        "Absolute Power",
+        "In the Line of Fire",
+        "Escape from Alcatraz",
+    ]
+    if args._sort:
+        crimes.sort()
+    if args._json:
+        print(json.dumps(crime, indent=4))
+    else:
+        print()
+        print("\n".join(crimes))
+        print()
+
+
+@movies.action(
+    "biography",
+    arguments={
+        "--json": json_flag_options,
+        "--sort": sort_flag_options,
+    },
+)
+def movies_biography(argv, args):
+    """
+    Biography movies starring Clint Eastwood
+    """
+    bios = [
+        "American Sniper",
+        "Sully",
+        "Changeling",
+        "Invictus",
+        "Bird",
+    ]
+    if args._sort:
+        bios.sort()
+    if args._json:
+        print(json.dumps(bios, indent=4))
+    else:
+        print()
+        print("\n".join(bios))
+        print()
+
+
